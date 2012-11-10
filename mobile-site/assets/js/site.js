bf8a7ca2b036b9a27bcce00e518fb184bef34ded
Peter Zhang <p@peetahzee.com>
2012-11-10 02:36:49 -0800

    news page

--- mobile-site/assets/js/site.js
+++ mobile-site/assets/js/site.js
@@ -4,6 +4,8 @@ $('document').ready(function() {
 		scalePanels();
 	});
 	
+	getWPPosts();
+	
 	var zIndex = 1;
 	$($(".full_height").get().reverse()).each(function() {
 		$(this).css("z-index", zIndex);
@@ -12,6 +14,27 @@ $('document').ready(function() {
 });
 
 function scalePanels() {
-	$(".full_height").height($(window).height()*0.8);
+	$(".full_height").height($(window).height()*0.85);
 	$(".full_height:last").height($(window).height());
+}
+
+function getWPPosts() {
+	$.getJSON('http://www.tamuseum.org/api/get_recent_posts/?json=1&callback=?', function(data) {
+		var i = 0;
+		$(data.posts).each(function() {
+			if(i > 5) return;
+			var content = stripEmptyP(stripImg(this.content));
+			content = content.match(/\<p(.*)\<\/p\>/ig)[0];
+			$("ul#news").append('<li><img src="'+this.thumbnail+'" /><h3>'+this.title+'</h3><p>'+content+'</p></li>');
+			
+			i++;
+		});
+	});
+}
+
+function stripImg(content) {
+	return content.replace(/\<img(.*)\>/ig, "");
+}
+function stripEmptyP(content) {
+	return content.replace(/\<p\>......\<\/p\>/ig, "");
 }
