b865d418740d39a94ed174b3f1ac7cf78cd83abe
Peter Zhang <p@peetahzee.com>
2012-11-10 04:25:54 -0800

    escape settings in js

--- mobile-site/assets/js/site.js
+++ mobile-site/assets/js/site.js
@@ -1,22 +1,29 @@
+const FULL_HEIGHT_PERCENTAGE = 0.85;
+const NEWS_SCROLL_DURATION = 500;
+const SLIDE_DURATION = 500; 
+const NEWS_TO_SHOW = 5;
+
 $(function(){
 
+	$(document).ready(function() {
+		getWPPosts();
+	
+		var zIndex = 1;
+		$($(".full_height").get().reverse()).each(function() {
+			$(this).css("z-index", zIndex);
+			zIndex++;
+		});
+	});
+	
 	$(window).resize(function() {
-		$(".full_height").css('min-height', $(window).height()*0.85);
+		$(".full_height").css('min-height', $(window).height() * FULL_HEIGHT_PERCENTAGE);
 		$(".full_height:last").css('min-height', $(window).height());
 	}).trigger('resize');
-	
-	getWPPosts();
-	
-	var zIndex = 1;
-	$($(".full_height").get().reverse()).each(function() {
-		$(this).css("z-index", zIndex);
-		zIndex++;
-	});
 
 	$('#main_nav a').click(function(evt){
 		evt.preventDefault();
 		if ($(this).is('#news')){
-			$('body').animate({scrollTop: $('#main_page_news').offset().top}, 500);
+			$('body').animate({scrollTop: $('#main_page_news').offset().top}, NEWS_SCROLL_DURATION);
 		} else {
 			//doNavigate($(this).attr('href'));
 			window.location.hash = '#!/' + $(this).attr('href');
@@ -27,7 +34,7 @@ $(function(){
 	function doNavigate(page){
 		$('#content_pane').load(page+' .wrapper', function(content){
 			$('#main_page_nav')
-				.animate({'margin-left': '-100%'}, 1000, function(){
+				.animate({'margin-left': '-100%'}, SLIDE_DURATION, function(){
 					$(this).height($('#content_pane .wrapper').height()).addClass('resized');
 				})
 		});
@@ -41,7 +48,7 @@ $(function(){
 			doNavigate(window.location.hash.substr(3));
 		} else {
 			$('#main_page_nav')
-				.animate({'margin-left': '0'}, 1000);
+				.animate({'margin-left': '0'}, SLIDE_DURATION);
 		}
 	}).trigger('hashchange');
 
@@ -57,9 +64,10 @@ function loadVideos(){
 
 function getWPPosts(){
 	$.getJSON('http://www.tamuseum.org/api/get_recent_posts/?json=1&callback=?', function(data) {
+		$("ul#news").html("");
 		var i = 0;
 		$(data.posts).each(function() {
-			if(i > 5) return;
+			if(i > NEWS_TO_SHOW) return;
 			var content = stripEmptyP(stripImg(this.content));
 			content = content.match(/\<p(.*)\<\/p\>/ig)[0];
 			$("ul#news").append('<a href="'+this.url+'"><li><img src="'+this.thumbnail+'" /><h3>'+this.title+'</h3><p>'+content+'</p></li></a>');
