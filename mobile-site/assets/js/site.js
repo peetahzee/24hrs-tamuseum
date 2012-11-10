ce3f238f725e09788225ae68ecffb1317101a84a
Peter Zhang <p@peetahzee.com>
2012-11-10 00:28:35 -0800

    resize panel on window resize

--- mobile-site/assets/js/site.js
+++ mobile-site/assets/js/site.js
@@ -1,5 +1,11 @@
 $('document').ready(function() {
+	scalePanels();
+	$(window).resize(function() {
+		scalePanels();
+	});
+});
+
+function scalePanels() {
 	$(".full_height").height($(window).height()*0.8);
 	$(".full_height:last").height($(window).height());
-	
-});
+}
